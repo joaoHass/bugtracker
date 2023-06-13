e8aa25adc1a028fc0144ed747f31f82a1ec045b0
joao.hass <joaopedrohass@outlook.com>
2023-06-13 00:30:56 -0300

    feat add getUserByEmail method on UserDomain

--- src/main/java/com/hass/bugtracker/domain/UserDomain.java
+++ src/main/java/com/hass/bugtracker/domain/UserDomain.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserDomain {
@@ -16,4 +17,6 @@ public class UserDomain {
     public UserDomain(UserRepository repository) { _repository = repository; }
 
     public List<User> getUsers() { return _repository.findAll(); }
+
+    public Optional<User> getUserByEmail(String email) { return _repository.findUserByEmail(email); }
 }
