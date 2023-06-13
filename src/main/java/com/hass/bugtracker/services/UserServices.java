f055c2ef4ae731aacd3bd84325614ade4b439dd1
joao.hass <joaopedrohass@outlook.com>
2023-06-13 00:32:11 -0300

    feat implement UserDetailsInterface on UserServices interface
    With the goal to utilize the database to store/retrieve user information

--- src/main/java/com/hass/bugtracker/services/UserServices.java
+++ src/main/java/com/hass/bugtracker/services/UserServices.java
@@ -3,10 +3,15 @@ package com.hass.bugtracker.services;
 import com.hass.bugtracker.domain.UserDomain;
 import com.hass.bugtracker.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class UserServices {
+@Service
+public class UserServices implements UserDetailsService {
 
     private final UserDomain _domain;
 
@@ -14,4 +19,11 @@ public class UserServices {
     public UserServices(UserDomain domain) { _domain = domain; }
 
     public List<User> getUsers() { return _domain.getUsers(); }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return _domain.getUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with email %s does not exist!", email)));
+    }
 }
