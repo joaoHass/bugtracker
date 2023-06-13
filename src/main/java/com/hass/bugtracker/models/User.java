d1a00aade742c5348225ec36c2d037ee0c832053
joao.hass <joaopedrohass@outlook.com>
2023-06-13 00:25:27 -0300

    feat implement UserDetail interface on User entity

--- src/main/java/com/hass/bugtracker/models/User.java
+++ src/main/java/com/hass/bugtracker/models/User.java
@@ -1,10 +1,14 @@
 package com.hass.bugtracker.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +25,8 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
+    @Override
+    public String getUsername() {
         return name;
     }
 
@@ -37,8 +42,33 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
