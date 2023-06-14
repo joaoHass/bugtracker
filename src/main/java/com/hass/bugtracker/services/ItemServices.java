8ac372aabe39b1a21ba3d0d7600a38a8ea5b98af
joao.hass <joaopedrohass@outlook.com>
2023-06-14 14:17:27 -0300

    feat add createItem method to ItemServices

--- src/main/java/com/hass/bugtracker/services/ItemServices.java
+++ src/main/java/com/hass/bugtracker/services/ItemServices.java
@@ -1,6 +1,7 @@
 package com.hass.bugtracker.services;
 
 import com.hass.bugtracker.domain.ItemDomain;
+import com.hass.bugtracker.dto.ItemDto;
 import com.hass.bugtracker.models.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,19 @@ public class ItemServices {
 
     public List<Item> getItems() {
         return _item.getItems();
+    }
+
+    public void createItem(ItemDto itemDto) throws IllegalArgumentException{
+        if (itemDto.title() == null)
+            throw new IllegalArgumentException("The item does not contain a title!");
+
+        Item item = new Item();
+        item.setTitle(itemDto.title());
+        item.setDescription(itemDto.description());
+        item.setDueDate(itemDto.dueDate());
+        item.setCompleted(itemDto.isCompleted());
+
+        _item.createItem(item);
+
     }
 }
