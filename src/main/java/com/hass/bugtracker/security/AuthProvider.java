11a2bb3f6c0bb38d1bbd5509dc894d68d1c397f8
joao.hass <joaopedrohass@outlook.com>
2023-06-23 17:44:36 -0300

    refactor comment AuthProvider code for the time being
    Until I have to actually use it

---
+++ src/main/java/com/hass/bugtracker/security/AuthProvider.java
@@ -0,0 +1,26 @@
+package com.hass.bugtracker.security;
+
+//@Component
+//public class AuthProvider implements AuthenticationProvider {
+//
+//    @Autowired
+//    private PasswordEncoder _passwordEncoder;
+//
+//    @Autowired
+//    private UserServices _userServices;
+//
+//    public AuthProvider(PasswordEncoder passwordEncoder, UserServices userServices) {
+//        _passwordEncoder = passwordEncoder;
+//        _userServices = userServices;
+//    }
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authentication) {
+//        return false;
+//    }
+//}
