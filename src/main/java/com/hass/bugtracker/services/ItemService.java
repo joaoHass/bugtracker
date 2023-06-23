79f708990653763868cae7b6994e2a2f7ae96e00
joao.hass <joaopedrohass@outlook.com>
2023-06-23 17:38:52 -0300

    fix createItem method from ItemService

--- src/main/java/com/hass/bugtracker/services/ItemService.java
+++ src/main/java/com/hass/bugtracker/services/ItemService.java
@@ -1,9 +1,12 @@
 package com.hass.bugtracker.services;
 
 import com.hass.bugtracker.domain.ItemDomain;
+import com.hass.bugtracker.domain.UserDomain;
 import com.hass.bugtracker.dto.ItemDto;
 import com.hass.bugtracker.models.Item;
+import com.hass.bugtracker.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,9 +16,13 @@ import java.util.List;
 public class ItemService {
 
     private final ItemDomain _item;
+    private final UserDomain _user;
 
     @Autowired
-    public ItemService(ItemDomain item) { _item = item; }
+    public ItemService(ItemDomain item, UserDomain user) {
+        _item = item;
+        _user = user;
+    }
 
     public List<ItemDto> getItems() {
 
@@ -38,17 +45,21 @@ public class ItemService {
         return itemsToReturn;
     }
 
-    public void createItem(ItemDto itemDto) throws IllegalArgumentException{
+    public void createItem(ItemDto itemDto, Authentication authentication) throws IllegalArgumentException {
         if (itemDto.title() == null)
             throw new IllegalArgumentException("The item does not contain a title!");
+
+        User loggedUser = _user.findById(((User)authentication.getPrincipal()).getId()).get();
 
         Item item = new Item();
         item.setTitle(itemDto.title());
         item.setDescription(itemDto.description());
         item.setDueDate(itemDto.dueDate());
+        item.setCreatedBy(loggedUser);
         item.setCompleted(itemDto.isCompleted());
 
         _item.createItem(item);
+    }
 
     }
 }
