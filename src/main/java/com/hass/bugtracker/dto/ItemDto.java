096f7ecdf4291fddebce22bf1e0e147deaaf2a74
joao.hass <joaopedrohass@outlook.com>
2023-06-23 17:40:06 -0300

    refactor add parameter createdByUserId to ItemDto

--- src/main/java/com/hass/bugtracker/dto/ItemDto.java
+++ src/main/java/com/hass/bugtracker/dto/ItemDto.java
@@ -2,5 +2,5 @@ package com.hass.bugtracker.dto;
 
 import java.time.LocalDate;
 
-public record ItemDto(String title, String description, LocalDate dueDate, boolean isCompleted) {
+public record ItemDto(String title, String description, LocalDate dueDate, Integer createdByUserId, boolean isCompleted) {
 }
